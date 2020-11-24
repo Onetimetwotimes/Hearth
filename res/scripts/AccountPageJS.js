1531b29ea3a44610797c6baa143a8c102a21bef3
Leonardo Contreras <LContreras@student.neumont.edu>
2020-11-23 18:02:39 -0700

    Account username and password now stored online. Link to data: https://api.apispreadsheets.com/data/3973/

---
+++ res/scripts/AccountPageJS.js
@@ -0,0 +1,30 @@
+﻿fetch("https://api.apispreadsheets.com/data/3973/").then(res => {
+    if (res.status === 200) {
+        // SUCCESS
+        res.json().then(data => {
+            const yourData = data
+        }).catch(err => console.log(err))
+    }
+    else {
+        // ERROR
+    }
+})
+
+function CreateAccount() {
+    $.ajax({
+        url: 'https://api.apispreadsheets.com/data/3973/',
+        type: 'post',
+        data: $("#myForm").serializeArray(),
+        success: function () {
+            alert("Successful")
+        },
+        error: function () {
+            alert("An Error Has Occurred")
+        }
+    });
+}
+
+function LogIn()
+    {
+        document.getElementById("headline").innerHTML = " Hello, (Person who signed in goes here)";
+    }
