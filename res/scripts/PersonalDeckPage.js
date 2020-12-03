c0b00c11397830e4fdfdf19165a7f1e13f9d49b5
yaakgold <yaakovgoldberg007@gmail.com>
2020-12-02 23:34:18 -0500

    Created the main card list portion of the deck building page
    I need to make it so that the cards can be added to the deck, and decks can be created and loaded and all of that fun stuff. There is also currently a bug that makes it so that when you click on a card, it will no longer go back to the :hover code, so that's a thing.

--- res/scripts/PersonalDeckPage.js
+++ res/scripts/PersonalDeckPage.js
@@ -1,12 +1,17 @@
+var currentPageNum = 1;
+var selectedCard = -1;
+
 //Remove this when done testing basics
-function showPage()
+function showPage(txtNameThing)
 {
-    switch(document.getElementById("testDrop").value)
+    switch(txtNameThing)
     {
         case "deckBuilder":
             $("#deckBuilder").show();
             $("#newDeckSetup").hide();
             $("#deckList").hide();
+
+            loadMoreCards({"region" : "us", "page" : currentPageNum});
             break;
         case "newDeckSetup":
             $("#deckBuilder").hide();
@@ -21,7 +26,71 @@ function showPage()
     }
 }
 
+function SelectCard(cardID)
+{
+    DeselectCard();
+    selectedCard = cardID;
+    $("#" + selectedCard).css("background-color", "darkgray");
+}
+
+function DeselectCard()
+{
+    if(selectedCard >= 0)
+    {
+        $("#" + selectedCard).css("background-color", "white");
+    }
+}
+
+function CardData(c)
+{
+    var retData = "" +
+    "<div id='" + c.id + "' class='cardDataSec' style='background-image: url(" + String.raw`${c.image}` + ");' onclick=SelectCard(" 
+    + c.id + ")" +
+    ">" +
+    // c.name +
+    "</div>";
+    
+
+    return retData;
+}
+
 function SetTitle()
 {
     document.title = document.getElementsByName("txt_DeckName")[0].value;
 }
+
+async function loadMoreCards(params)
+{
+    let cardList;
+    while(!cardList)
+    {
+        cardList = await Promise.resolve(cardsearch(params));
+    }
+
+    cardList = cardList.data;
+    console.log(cardList);
+
+    if(currentPageNum < cardList.pageCount);
+    cardList.cards.forEach(c => {
+        if(c.cardTypeId != 3)
+        {
+            $("#cardListSection").html($("#cardListSection").html() + CardData(c));
+        }
+    });
+
+    currentPageNum++;
+}
+
+
+function AddCardToDeck(cardID)
+{
+
+}
+
+$(window).scroll(function() {
+    //console.log(Math.ceil($(window).scrollTop() + $(window).height()) + " " + $(document).height());
+    if(Math.ceil($(window).scrollTop() + $(window).height()) >= $(document).height())
+    {
+        loadMoreCards({"region" : "us", "page" : currentPageNum});
+    }
+ });
