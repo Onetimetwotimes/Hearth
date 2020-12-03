def5e400745394ad78cdd54f700b9e324f543f06
Leonardo Contreras <LContreras@student.neumont.edu>
2020-12-03 11:52:23 -0700

    User cannot enter any empty fields. User can properly enter a correct username and Passwords(Improvements still needed)

--- res/scripts/AccountPageJS.js
+++ res/scripts/AccountPageJS.js
@@ -1,30 +1,53 @@
-﻿fetch("https://api.apispreadsheets.com/data/3973/").then(res => {
-    if (res.status === 200) {
-        // SUCCESS
-        res.json().then(data => {
-            const yourData = data
-        }).catch(err => console.log(err))
-    }
-    else {
-        // ERROR
-    }
-})
+﻿
+var user;
+var pass;
 
 function CreateAccount() {
-    $.ajax({
-        url: 'https://api.apispreadsheets.com/data/3973/',
-        type: 'post',
-        data: $("#myForm").serializeArray(),
-        success: function () {
-            alert("Successful")
-        },
-        error: function () {
-            alert("An Error Has Occurred")
-        }
-    });
+
+    user = document.getElementsByName("Username")[0].value;
+    pass = document.getElementsByName("Password")[0].value;
+
+    if (user && pass) {
+
+
+        $.ajax({
+            url: 'https://api.apispreadsheets.com/data/3973/',
+            type: 'post',
+            data: $("#myForm").serializeArray(),
+            success: function () {
+                alert("Successful")
+            },
+            error: function () {
+                alert("An Error Has Occurred")
+            }
+        });
+    }
+    else {
+        document.getElementById("headline").innerHTML = "Please fill in all the empty fields"
+    }
 }
 
 function LogIn()
-    {
-        document.getElementById("headline").innerHTML = " Hello, (Person who signed in goes here)";
+{
+        user = document.getElementsByName("Username")[0].value;
+        pass = document.getElementsByName("Password")[0].value;
+    if (user && pass) {
+        fetch("https://api.apispreadsheets.com/data/3973/?query=select*from3973whereUsername='" + user + "'ANDPassword='" + pass + "'").then(res => {
+            if (res.status === 200) {
+                // SUCCESS
+                res.json().then(data => {
+                    const yourData = data
+                    console.log(yourData)
+                    document.getElementById("headline").innerHTML = " Hello, " + user;
+                }).catch(err => console.log(err))
+            }
+            else {
+                // ERROR
+            }
+        })
     }
+    else
+    {
+        document.getElementById("headline").innerHTML = "Please fill in all the empty fields"
+    }
+}
